09d0bfe3d691514e7bde28d9ed90a58bbbf7dd23
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-01-25 02:19:54 +0545

    fetch data...

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-async function fetchData() {
-    //     const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data");
-    //     const record = await res.json();
-    //     document.getElementById("date").innerHTML = record.data[0].date;
-    //     document.getElementById("areaName").innerHTML = record.data[0].areaName;
-    //     document.getElementById("latestBy").innerHTML = record.data[0].latestBy;
-    //     document.getElementById("deathNew").innerHTML = record.data[0].deathNew;
-}
-fetchData();
+const baseCoinAPI = "https://pro-api.coinmarketcap.com/";
+const apiVersion = "v1";
+const apiKey = `&CMC_PRO_API_KEY=${REACT_APP_CMC_KEY}`;
+
+//Function to get the top 25 coins.
+const getCoins = async (endPoint) => {
+    let path = `${baseCoinAPI}${apiVersion}${endPoint}${apiKey}`;
+    const fetchResult = await fetch(path);
+    const result = await fetchResult.json();
+    console.log(result);
+
+    if (fetchResult.ok) {
+        return result;
+    }
+    const responseError = {
+        type: "Error",
+        message: result.message || "Something went wrong",
+        data: result.data || "",
+        code: result.code || "",
+    };
+
+    let error = new Error();
+    error = { ...error, ...responseError };
+    throw error;
+};
+
+//DOM Manipulation to show all the coins and respective values
+const coins = getCoins("/cryptocurrency/listings/latest?limit=25");
