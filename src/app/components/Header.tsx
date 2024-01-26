1cfe84efe0ca801f6e259e3969c37aa832245c04
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-01-27 02:04:42 +0545

    create header and footer component.

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+export const Header: FC = () => {
+    return (
+        <header>
+            <h1 className="title">RupeeSync Crypto Monitor</h1>
+        </header>
+    );
+};
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
 import React, { FC } from "react";
+import { Header } from "./components/Header";
+import Footer from "./components/Footer";
 
 const App: FC = () => {
     return (
-        <div>
-            <h1>App </h1>
+        <div className="pradip_coin_container">
+            <Header />
+            <div className="content">
+                <h1>BTC</h1>
+                <h4>Rs. 5,279,922.69</h4>
+            </div>
+            <Footer />
         </div>
     );
 };
